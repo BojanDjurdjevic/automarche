68cbf73ff770843595dd79e6f3a1497afbf7a0b4
BojanDjurdjevic <pininfarina164@gmail.com>
2024-10-10 09:38:06 +0200

    addCar form

--- addcar.php
+++ addcar.php
@@ -13,7 +13,7 @@ require_once "required/_required.php";
     <?php require_once "components/_header.php"; ?>
     <main>
         <div class="main">
-            <form action="addcar.php">
+            <form action="addcar.php" method="post" id="form" enctype="multipart/form-data">
                 <input type="text" name="make" placeholder="Make">
                 <input type="text" name="model" placeholder="Model">
                 <input type="number" name="price" placeholder="Price">
@@ -26,6 +26,34 @@ require_once "required/_required.php";
                     <option value="Wagon">Wagon</option>
                     <option value="Convertible">Convertible</option>
                 </select>
+                <select name="fuel" id="fuel">
+                    <option value="0">--Chose the fuel--</option>
+                    <option value="Gasoline">Gasoline</option>
+                    <option value="Diesel">Diesel</option>
+                    <option value="TNG">TNG</option>
+                    <option value="CNG">CNG</option>
+                    <option value="Hybrid">Hybrid</option>
+                    <option value="Electric">Electric</option>
+                </select>
+                <input type="number" name="power" placeholder="Engine power">
+                <input type="number" name="engine" placeholder="Engine size">
+                <input type="number" name="km" placeholder="Km">
+                <select name="gear" id="gear">
+                    <option value="0">--Chose the gear--</option>
+                    <option value="Manual">Manual</option>
+                    <option value="Automatic">Automatic</option>
+                </select>
+                <input type="number" name="doors" placeholder="Number of doors">
+                <input type="number" name="seats" placeholder="Number of seats">
+                <input type="text" name="color" placeholder="Color">
+                <select name="wheel" id="wheel">
+                    <option value="0">Chose the wheel drive--</option>
+                    <option value="Front">Front</option>
+                    <option value="Rear">Rear</option>
+                </select>
+                <input type="date" name="regdate" placeholder="Registration expiry">
+                <textarea name="description" id="description" placeholder="Add a description of your car"></textarea>
+                <button>Add</button>
             </form>
         </div>
     </main>
